dedca4d65b22e2308edc33cc349199289343cb75
y.jacquet <ysee.jacquet@etu.unistra.fr>
2024-01-10 18:24:31 +0100

    Update hashmap tests

--- test/test_hashmap.c
+++ test/test_hashmap.c
@@ -4,29 +4,83 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-void hashmap_running(void)
+void h_create(void)
 {
     Hashmap *h = hashmap_create(4);
     TEST_ASSERT_NOT_NULL(h);
     TEST_ASSERT_NOT_NULL(h->items);
-    TEST_ASSERT_EQUAL(0, hashmap_get(h,10));
+    hashmap_free(h);
+}
+
+void h_insert(void)
+{
+    Hashmap *h = hashmap_create(4);
+    TEST_ASSERT_EQUAL(0, h->items[1].key);
+    TEST_ASSERT_EQUAL(0, h->items[1].value);
+    hashmap_insert(h,1,2);
+    TEST_ASSERT_EQUAL(1, h->items[1].key);
+    TEST_ASSERT_EQUAL(2, h->items[1].value);
+    hashmap_insert(h,1,2);
+    hashmap_insert(h,1,2);
+    hashmap_insert(h,1,2);
+    TEST_ASSERT_EQUAL(4, h->size);
+    hashmap_free(h);
+}
+
+void h_find(void)
+{
+    Hashmap *h = hashmap_create(4);
+    TEST_ASSERT_EQUAL(-1, hashmap_find(h,5));
     hashmap_insert(h,1,2);
     hashmap_insert(h,5,10);
     TEST_ASSERT_EQUAL(2, hashmap_find(h,5));
+    hashmap_free(h);
+}
+
+void h_get(void)
+{
+    Hashmap *h = hashmap_create(4);
+    TEST_ASSERT_EQUAL(0, hashmap_get(h,1));
+    hashmap_insert(h,1,2);
+    TEST_ASSERT_EQUAL(2, hashmap_get(h,1));
+    hashmap_free(h);
+}
+
+void h_resize(void)
+{
+    Hashmap *h = hashmap_create(4);
+    hashmap_insert(h,1,2);
+    hashmap_insert(h,5,10);
     hashmap_insert(h,4,8);
     hashmap_insert(h,7,14);
     TEST_ASSERT_EQUAL(8,h->size);
     TEST_ASSERT_EQUAL(5, hashmap_find(h,5));
     TEST_ASSERT_EQUAL(10, hashmap_get(h,5));
+    hashmap_free(h);
+}
+
+void h_remove(void)
+{
+    Hashmap *h = hashmap_create(4);
+    hashmap_insert(h,1,2);
+    hashmap_insert(h,5,10);
+    hashmap_insert(h,4,8);
+    hashmap_insert(h,7,14);
     uint r = hashmap_remove(h,5);
     TEST_ASSERT_EQUAL(10,r);
     TEST_ASSERT_EQUAL(TOMBSTONE,h->items[5].key);
     hashmap_free(h);
 }
 
+
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(hashmap_running);
+    RUN_TEST(h_create);
+    RUN_TEST(h_insert);
+    RUN_TEST(h_find);
+    RUN_TEST(h_get);
+    RUN_TEST(h_resize);
+    RUN_TEST(h_remove);
     return UNITY_END();
 }

76e63eca6dc8fcb5779c9affe195bf7c882c6e97
y.jacquet <ysee.jacquet@etu.unistra.fr>
2024-01-21 00:38:06 +0100

    Update hashmap.h documentation

--- include/hashmap.h
+++ include/hashmap.h
@@ -4,95 +4,112 @@
 
 typedef unsigned int uint;
 
+/** Default value given to each Item in the Hashmap.*/
 #define EMPTY 0
+/** Value given to each removed Item of the Hashmap. */
 #define TOMBSTONE 4294967295
 
+/**
+ * @brief Standard structure for an Item of a Hashmap.
+ */
 typedef struct Item {
-    uint key;
-    uint value;
+    /*@{*/
+    uint key; /**< Value hashed to find the index of the Item. */
+    uint value; /**< Stored value. */
+    /*@}*/
 } Item;
 
+/**
+ * @brief Standard structure for a Hashmap.
+ */
 typedef struct Hashmap {
-    Item* items;
-    uint size;
-    uint count;
+    /*@{*/
+    Item* items; /**< Array of Item structures. */
+    uint size; /**<  Length of Hashmap::items. */
+    uint count; /**< Number of filled indexes in Hashmap::items. */
+    /*@}*/
 } Hashmap;
 
 /**
- * @brief Create a hashmap with an initial size.
+ * @brief Create a Hashmap with an initial \p size.
  * 
- * @param size Initial size of the hashmap.
- * @return Hashmap* A pointer to the created hashmap.
+ * @param size Initial size of the Hashmap (Hashmap::size).
+ * @return `Hashmap*`: A pointer to the created Hashmap.
  */
 Hashmap* hashmap_create(uint size);
 
 /**
- * @brief Free the hashmap.
+ * @brief Free the given Hashmap \p h.
  * 
- * @param h The hashmap to free.
+ * @param h The Hashmap to free.
  */
 void hashmap_free(Hashmap* h);
 
 /**
- * @brief Resize the given hashmap by 2.
+ * @brief Resize the given Hashmap \p h by \p new_size.
  * 
- * @param h The hashmap to resize.
- * @param new_size New size for the hashmap.
+ * @param h The Hashmap to resize.
+ * @param new_size New size for the Hashmap.
  */
 void hashmap_resize(Hashmap* h, uint new_size);
 
 /**
- * @brief Find the index of a given key in the hashmap.
+ * @brief Find the index of a given \p key in the Hashmap \p h.
  * 
- * @param h The given hashmap.
- * @param key The key to find.
- * @return int -1 if the key doesn't exist, its index otherwise.
+ * @param h The given Hashmap.
+ * @param key The Item::key to find.
+ * @return `int`: -1 if the \p key doesn't exist, its index otherwise.
  */
 int hashmap_find(Hashmap* h, uint key);
 
 /**
- * @brief Find the value associated to he given key in the hashmap.
+ * @brief Find the value associated to the given \p key in the Hashmap \p h.
  * 
- * @param h  The given hashmap.
- * @param key The key we are looking for.
- * @return uint The value associated to the key.
+ * @param h  The given Hashmap.
+ * @param key The Item::key we are looking for.
+ * @return `uint`: The Item::value associated to the Item::key.
  */
 uint hashmap_get(Hashmap* h, uint key);
 
 /**
- * @brief Insert a new element in the hashmap, if it doesn't already exist.
+ * @brief Insert a new element in the Hashmap \p h, if it doesn't already exist.
  * 
- * @param h The given hashmap.
- * @param key The key to add.
- * @param value The associated value.
+ * @param h The given Hashmap.
+ * @param key The Item::key to add.
+ * @param value The associated Item::value.
  */
 void hashmap_insert(Hashmap* h, uint key, uint value);
 
 /**
- * @brief Set the value of key to new_value.
- * @note If the key doesn't exist, use `hashmap_insert` to create it.
+ * @brief Increase the value of \p key in the Hashmap `h` by \p incr.
+ * @note If the \p key doesn't exist, use @ref hashmap_insert to create it.
  * 
- * @param h The given hashmap.
- * @param key The key to find.
- * @param incr The desiredd increase of the value.
+ * @param h The given Hashmap.
+ * @param key The Item::key to find.
+ * @param incr The desired increase of the tem::value.
  */
 void hashmap_increase(Hashmap* h, uint key, uint incr);
 
 /**
- * @brief Remove the item with the corresponding key from the hashmap.
+ * @brief Remove the item with the corresponding \p key from the hashmap \p h.
  * 
- * @param h The given hashmap.
- * @param key The key to remove.
- * @return uint The removed key.
+ * @param h The given Hashmap.
+ * @param key The Item::key to remove.
+ * @return `uint`: The removed Item::key.
  */
 uint hashmap_remove(Hashmap* h, uint key);
 
 /**
- * @brief Write the given hashmap to a file.
+ * @brief Write the given hashmap \p h to the file \p file.
+ * 
+ * @param h The given Hashmap.
+ * @param file The file where writte the Hashmap.
 */
 void write_hashmap_to_file(Hashmap *h, FILE *file);
 
 /**
- * @brief Read a hashmap from a file.
+ * @brief Read a Hashmap from the file \p file
+ * 
+ * @param file The file to extract the Hashmap.
 */
 Hashmap* read_hashmap_from_file(FILE *file);
